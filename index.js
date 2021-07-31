ed04fc74cae65419be05ae5740234913f9a617b5
Dipankar Mishra <mishradipankar61@gmail.com>
2021-08-01 01:29:02 +0530

    assignment

--- index.js
+++ index.js
@@ -2,48 +2,22 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const { RSA_SSLV23_PADDING } = require('constants');
+const dishRouter=require('./routes/dishRouter');
+const promoRouter=require('./routes/promoRouter');
+const leaderRouter=require('./routes/leaderRouter');
+
 
 const hostname = 'localhost';
 const port = '3000';
+
 const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes',(req,res,next)=>{
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-});
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
 
-app.get('/dishes',(req,res,next) => {
-    res.end('will send all dishes to you!');
-});
-app.post('/dishes',(req,res,next) => {
-    res.end('will add the dish'+req.body.name+'with details'+req.body.description);
-});
-app.put('/dishes',(req,res,next) => {
-    res.statusCode=403;
-    res.end('Put operation not supported in /dishes');
-});
-app.delete('/dishes',(req,res,next) => {
-    res.end('deleting all the dishes!');
-});
-app.get('/dishes/:dishId',(req,res,next) => {
-    res.end('will send details of the dish'+req.params.dishId+'to you!');
-});
-app.post('/dishes/:dishId',(req,res,next) => {
-    res.statusCode=403;
-    res.end('Post operation not supported in /dishes/'+req.params.dishId);
-
-});
-app.put('/dishes/:dishId',(req,res,next) => {
-    res.write('updating the dish'+req.params.dishId+'\n');
-    res.end('Will update the dishes'+req.body.name+'with details:'+req.body.description);
-});
-app.delete('/dishes/:dishId',(req,res,next) => {
-    res.end('deleting dishe!'+req.params.dishId);
-});
+app.use('/leaders',leaderRouter);
 
 
 app.use(express.static(__dirname + '/public'));
@@ -56,6 +30,6 @@ app.use((req,res,next)=>{
 
 const server = http.createServer(app);
 server.listen(port,hostname,()=>{
-    console.log(`server running at http://${hostname}:${port}`);
+    console.log(`Server running at http://${hostname}:${port}`);
 
 });
